2452495e66f4f110360d85f49d2cf54cb911bfb3
Appeltabak <tonnisoosterhoff@gmail.com>
2015-09-17 23:16:31 +0200

    #12 Removed unnecessary db session statements from app.__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,5 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -9,9 +7,5 @@ db = SQLAlchemy(app)
 
 from app.models import User
 from app import views
-
-engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'], echo=True)
-Session = sessionmaker(bind=engine)
-session = Session()
 
 db.create_all()
