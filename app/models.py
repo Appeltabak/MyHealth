0d2736c8c01006d01859a19466c4290def7e6ccd
PepijnCompaan <pepijncompaan@hotmail.com>
2015-09-17 16:04:29 +0200

    Chanded table name of user in model.

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from app import db
 
 class User(db.Model):
+    __tablename__ = "mha_user"
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
