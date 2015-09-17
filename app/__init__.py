b78302a554a9999317f3d4ac933d76bbe7df1ce9
PepijnCompaan <pepijncompaan@hotmail.com>
2015-09-17 12:16:04 +0200

    #11 Started organisation, basic file and folder structure setup.

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,17 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config.from_object('config')
 db = SQLAlchemy(app)
 
 from app.models import User
+from app import views
 
-engine = create_engine(os.environ.get('DATABASE_URL'), echo=True)
+engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'], echo=True)
 Session = sessionmaker(bind=engine)
 session = Session()
 
 db.create_all()
-
-admin = User('admin', 'test@test.com')
-db.session.add(admin)
-db.session.commit()
-users = User.query.all()
-print users
--- server.py
+++ server.py
@@ -1,9 +1,4 @@
 from app import app
 
-
-@app.route('/')
-def hello():
-    return 'Hello World!!'
-
 if __name__ == "__main__":
     app.run()
