ae547cc01b2724c81a43752d81275974e4e321d1
PepijnCompaan <pepijncompaan@hotmail.com>
2015-09-17 14:52:21 +0200

    Added db connection test.

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import database_exists, create_database
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -11,6 +12,9 @@ from app.models import User
 from app import views
 
 engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'], echo=True)
+if not database_exists(engine.url):
+        print "Database not found, creating new..."
+        create_database(engine.url)
 Session = sessionmaker(bind=engine)
 session = Session()
 
---
+++ testdatabaseconnection.py
@@ -0,0 +1,38 @@
+from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils.functions import database_exists, create_database
+
+
+def init(app):
+    print "starting db test..."
+    db = SQLAlchemy(app)
+
+    from app.models import User
+
+    engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'], echo=False)
+    if not database_exists(engine.url):
+        print "Database not found, creating new..."
+        create_database(engine.url)
+        print "Done!"
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    db.create_all()
+
+    print "creating user 'admin'..."
+    admin = User("admin", "test@test.com")
+    session.add(admin)
+    session.commit()
+    print "done!"
+
+    print "Users found in database:"
+    for instance in session.query(User):
+        print instance.username
+        print "destroying user..."
+        session.delete(instance)
+        session.commit()
+        print "done!"
+
+    print "db test done!"
